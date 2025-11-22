efa0496de77b023c37adea3fdd86d92d16a2bc42
James McDonald <jamesmcdonald112@yahoo.ie>
2025-11-22 17:40:28 +0000

    refactor(cart): getFromLocalStorage takes a string for params

--- app/features/cart/utils/cart-storage.ts
+++ app/features/cart/utils/cart-storage.ts
@@ -3,22 +3,21 @@ interface addToLocalStorageProps<T> {
   value: T;
 }
 
-export function addToLocalStorage<T>({ key, value}: addToLocalStorageProps<T>): void {
+export function addToLocalStorage<T>({
+  key,
+  value,
+}: addToLocalStorageProps<T>): void {
   try {
     localStorage.setItem(key, JSON.stringify(value));
-  } catch(error: unknown) {
+  } catch (error: unknown) {
     console.log("Error adding to local storage: ", error);
   }
 }
 
-interface getFromLocalStorageProps {
-  key: string;
-}
-
-export function getFromLocalStorage<T>({key}: getFromLocalStorageProps): T | null {
+export function getFromLocalStorage<T>(key: string): T | null {
   try {
     const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) as T : null;
+    return item ? (JSON.parse(item) as T) : null;
   } catch (error: unknown) {
     console.log("Error reading from local storage: ", error);
     return null;
