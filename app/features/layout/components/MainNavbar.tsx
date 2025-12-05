dae1c94c3ff903a99e5bacd287783f5ca98a0c70
James McDonald <jamesmcdonald112@yahoo.ie>
2025-12-05 19:29:07 +0000

    feat(layout): add main navbar with navigation menu

---
+++ app/features/layout/components/MainNavbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+export function MainNavbar() {
+	return (
+		<header className="border-b bg-background">
+			<div className=" mx-auto flex h-16 max-w-7xl items-center justify-between px-4">
+				{/* LOGO */}
+				<Link href="/" className="text-xl font-bold">
+					MyStore
+				</Link>
+
+				{/* NAV LINKS */}
+				<NavigationMenu>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<NavigationMenuLink href="/products">Products</NavigationMenuLink>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem>
+							<NavigationMenuLink href="/cart">Cart</NavigationMenuLink>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem>
+							<NavigationMenuLink href="/about">About</NavigationMenuLink>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+		</header>
+	);
+}
