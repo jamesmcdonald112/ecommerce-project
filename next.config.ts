656d140955a52868941c260130c03552f0e710ff
James McDonald <jamesmcdonald112@yahoo.ie>
2025-12-06 14:48:50 +0000

    chore(config): add redirect from /about to home

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "plus.unsplash.com",
-				port: "",
-				pathname: "/**",
-			},
-			// add more allowed domains here later
-		],
-	},
+  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      // add more allowed domains here later
+    ],
+  },
 };
 
 export default nextConfig;
